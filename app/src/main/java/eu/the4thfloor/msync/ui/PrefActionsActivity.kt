9d8156d2737078899be7ce567251b233c8b3c4c4
Ralph Bergmann <ralph@the4thfloor.eu>
2017-11-08 00:48:49 +0100

    add "send debug infos"

--- app/src/main/java/eu/the4thfloor/msync/ui/PrefActionsActivity.kt
+++ app/src/main/java/eu/the4thfloor/msync/ui/PrefActionsActivity.kt
@@ -18,9 +18,13 @@ package eu.the4thfloor.msync.ui
 
 import android.app.Activity
 import android.content.Intent
+import android.content.res.Resources
+import android.os.Build
 import android.os.Bundle
+import eu.the4thfloor.msync.BuildConfig
 import eu.the4thfloor.msync.BuildConfig.GITHUB_LINK
 import eu.the4thfloor.msync.BuildConfig.PLAYSTORE_LINK
+import eu.the4thfloor.msync.BuildConfig.PREF_ACTION_DEBUG
 import eu.the4thfloor.msync.BuildConfig.PREF_ACTION_GITHUB
 import eu.the4thfloor.msync.BuildConfig.PREF_ACTION_LOGOUT
 import eu.the4thfloor.msync.BuildConfig.PREF_ACTION_SHARE
@@ -28,7 +32,11 @@ import eu.the4thfloor.msync.BuildConfig.PREF_ACTION_SYNC_NOW
 import eu.the4thfloor.msync.MSyncApp
 import eu.the4thfloor.msync.utils.createSyncJobs
 import eu.the4thfloor.msync.utils.deleteAccount
+import eu.the4thfloor.msync.utils.getRefreshToken
 import org.jetbrains.anko.browse
+import org.jetbrains.anko.defaultSharedPreferences
+import org.jetbrains.anko.email
+import java.util.*
 
 
 class PrefActionsActivity : Activity() {
@@ -39,25 +47,40 @@ class PrefActionsActivity : Activity() {
         MSyncApp.graph.inject(this)
 
         when (intent?.dataString) {
-            PREF_ACTION_SYNC_NOW -> {
-                createSyncJobs(true)
-            }
-            PREF_ACTION_SHARE    -> {
-                startActivity(Intent.createChooser(Intent(Intent.ACTION_SEND)
-                                                       .apply {
-                                                           type = "text/plain"
-                                                           putExtra(Intent.EXTRA_TEXT, PLAYSTORE_LINK)
-                                                       },
-                                                   null))
-            }
+            PREF_ACTION_SYNC_NOW -> createSyncJobs(true)
+            PREF_ACTION_SHARE    -> startActivity(Intent.createChooser(Intent(Intent.ACTION_SEND)
+                                                                           .apply {
+                                                                               type = "text/plain"
+                                                                               putExtra(Intent.EXTRA_TEXT, PLAYSTORE_LINK)
+                                                                           },
+                                                                       null))
             PREF_ACTION_LOGOUT   -> {
                 deleteAccount()
                 finishAffinity()
             }
+            PREF_ACTION_DEBUG    -> sendDebugInfos()
             PREF_ACTION_GITHUB   -> browse(GITHUB_LINK, true)
             else                 -> { }
         }
 
         finish()
     }
+
+    private fun sendDebugInfos() =
+        email("ralph@the4thfloor.eu",
+              "Msync Debug Infos",
+              "Android Version: ${Build.VERSION.RELEASE}\n" +
+              "Android Api Level: ${Build.VERSION.SDK_INT}\n" +
+              "Device: ${Build.MODEL}\n" +
+              "Manufacturer: ${Build.MANUFACTURER}\n" +
+              "System Language: ${Resources.getSystem().configuration.locale.language}\n" +
+              "App Language: ${Locale.getDefault().language}\n" +
+              "App Version: ${BuildConfig.VERSION_NAME}\n" +
+              "Git commit: ${BuildConfig.GIT_SHA}\n" +
+              "Sync yes: ${defaultSharedPreferences.getBoolean("pref_key_sync_event_yes", true)}\n" +
+              "Sync waitlist: ${defaultSharedPreferences.getBoolean("pref_key_sync_event_waitlist", true)}\n" +
+              "Sync unanswered: ${defaultSharedPreferences.getBoolean("pref_key_sync_event_unanswered", false)}\n" +
+              "Sync no: ${defaultSharedPreferences.getBoolean("pref_key_sync_event_no", false)}\n" +
+              "Sync frequency: ${defaultSharedPreferences.getString("pref_key_sync_frequency", "1440")}\n" +
+              "Token: ${getRefreshToken()}")
 }
